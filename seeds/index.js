4647e6fe1df325bdb2f5f6213214a11d8aa13e38
Richard Goater <rg19@sanger.ac.uk>
2021-11-25 19:30:21 +0000

    Edit seed script

--- seeds/index.js
+++ seeds/index.js
@@ -2,9 +2,10 @@ const fs = require('fs')
 const mapTsvToRecords = require('../mapTsvToRecords')
 
 exports.seed = async function (knex) {
-  const text = fs.readFileSync('./input.tsv', 'utf8')
+  const text = fs.readFileSync('./full_data_table.csv', 'utf8')
 
   const records = mapTsvToRecords(text)
+
   console.log('Mapped', records.length, 'records, inserting ...')
   await knex('aggregated').del()
   await knex.batchInsert('aggregated', records)
